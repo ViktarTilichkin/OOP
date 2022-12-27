08f79d9fcbde9cf9ba40ca9290a4da7691140830
ViktarTilichkin <tilichkin@mail.ru>
2022-12-27 22:08:15 +0300

    update

--- TaskIntro/MyClasses/Fibonacci.cs
+++ TaskIntro/MyClasses/Fibonacci.cs
@@ -7,7 +7,7 @@ namespace TaskIntro.MyClasses
 
         private int Index;
         private ulong[] TempFib = new ulong[2] { 0, 1 };
-        private ulong[] ResultFib; 
+        private ulong[] ResultFib;
         public Fibonacci()
         {
             Index = 1;
@@ -15,11 +15,11 @@ namespace TaskIntro.MyClasses
         }
         public Fibonacci(int length)
         {
-            if ( length > 1)
+            if (length > 1)
             {
-            Index = length;
-            ResultFib = new ulong[length];
-            Array.Copy(TempFib, ResultFib, TempFib.Length);
+                Index = length;
+                ResultFib = new ulong[length];
+                Array.Copy(TempFib, ResultFib, TempFib.Length);
             }
             else
             {
@@ -30,9 +30,9 @@ namespace TaskIntro.MyClasses
 
         public void PrintFib()
         {
-            for (int i = 2; i < Index ; i++ )
+            for (int i = 2; i < Index; i++)
             {
-                ResultFib[i] = ResultFib[i-1] + ResultFib[i-2];
+                ResultFib[i] = ResultFib[i - 1] + ResultFib[i - 2];
             }
             Console.WriteLine(string.Join(", ", ResultFib));
             Console.WriteLine("hello Fibonacci");
