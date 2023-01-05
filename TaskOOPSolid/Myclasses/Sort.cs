61d398322ed62ab19e1823128eb12479c68c9232
ViktarTilichkin <tilichkin@mail.ru>
2023-01-05 10:11:03 +0300

    add Sort.cs

--- TaskOOPSolid/Myclasses/Sort.cs
+++ TaskOOPSolid/Myclasses/Sort.cs
@@ -2,3 +2,23 @@
 // статичного массива сортировкой пузырьком
 
 namespace Myclasses.Sort;
+
+public class Sort
+{
+    public void Sort(out int[] numbers, params int[] num)
+    {
+        for (int i = 0; i < num.Length - 1; i++)
+        {
+            for (int j = 0; j < num.Length - 1; j++)
+            {
+                if (num[j] > num[j + 1])
+                {
+                    int temp = num[j];
+                    num[j] = num[j+1];
+                    num[j+1]= temp;
+                }
+            }
+        }
+    }
+
+}
