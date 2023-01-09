888e81338a10aa39dcdaf25d278b050e4179aac9
ViktarTilichkin <tilichkin@mail.ru>
2023-01-09 22:36:16 +0300

    update

--- TaskOOPInheritance/Program.cs
+++ TaskOOPInheritance/Program.cs
@@ -9,30 +9,30 @@ namespace MyApp // Note: actual namespace depends on the project name.
 
         static void Main(string[] args)
         {
-            // Transport obj = new Transport(20, "BIGCAR");
-            // // obj.Moving();
-            // // obj.Stopping();
-            // Car obj1 = new Car("brichka", 66);
-            // // obj1.Moving();
-            // // obj1.Stopping();
-            // // obj1.Drift();
-            // Car obj2 = new Car("kek", 22);
-            // Transport[] array = new Transport[3] {obj, obj2, obj1};
+            Transport obj = new Transport(20, "BIGCAR");
+            // obj.Moving();
+            // obj.Stopping();
+            Car obj1 = new Car("brichka", 66);
+            // obj1.Moving();
+            // obj1.Stopping();
+            // obj1.Drift();
+            Car obj2 = new Car("kek", 22);
+            Transport[] array = new Transport[3] {obj, obj2, obj1};
 
-            // foreach(Transport item in array)
-            // {
-            //     if (item is Car car)
-            //     {
-            //         Console.WriteLine("this is car");
-            //         car.Drift();
-            //     }
-            //     if (item is Transport)
-            //     {
-            //         Console.WriteLine("this is transport");
-            //     }
-            //     item.Moving();
-            //     item.Stopping();
-            // }
+            foreach(Transport item in array)
+            {
+                if (item is Car car)
+                {
+                    Console.WriteLine("this is car");
+                    car.Drift();
+                }
+                if (item is Transport)
+                {
+                    Console.WriteLine("this is transport");
+                }
+                item.Moving();
+                item.Stopping();
+            }
             Figur figura = new Figur("null");
             figura.Square();
             Figur rectangle = new Rectangle("pr9mougolnik", 5, 10);
