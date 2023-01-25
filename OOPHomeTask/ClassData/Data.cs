e96997bb69bcf08318727601406381a20699b867
ViktarTilichkin <tilichkin@mail.ru>
2023-01-25 21:10:12 +0300

    update

--- OOPHomeTask/ClassData/Data.cs
+++ OOPHomeTask/ClassData/Data.cs
@@ -2,7 +2,7 @@ namespace ClassData;
 // Задача
 // Написать обобщенный класс (без ограничений)
 // поле Items массив 
-// метод добавления
+// метод добавления+
 // метод поиск индеса по значению
 // удаления значения (по индексу, удаление по занчению)
 // метод вывода
@@ -27,14 +27,53 @@ public class DbUser
         ArrayUsers[id - 1].Age = age;
         return true;
     }
+    public User? Search(string name)
+    {
+        if (!string.IsNullOrEmpty(name))
+        {
+            name.Trim();
+            for (int i = 0; i < ArrayUsers.Length; i++)
+            {
+                if (name.ToLower().Equals(ArrayUsers[i].Name.ToLower()))
+                {
+                    return ArrayUsers[i];
+                }
+            }
+        }
+        return null;
+    }
     public bool RemoveById(int id)
     {
         ArrayUsers[id - 1].Name = "dafault";
         ArrayUsers[id - 1].Age = 0;
         return true;
     }
-    public bool RemoveByValue()
+    public bool RemoveByValue(string name)
     {
+        if (!string.IsNullOrEmpty(name))
+        {
+            name.Trim();
+            for (int i = 0; i < ArrayUsers.Length; i++)
+            {
+                if (name.ToLower().Equals(ArrayUsers[i].Name.ToLower()))
+                {
+                    ArrayUsers[i].Name = "defaul";
+                    ArrayUsers[i].Age = 0;
+                }
+            }
+        }
+        return true;
+    }
+    public bool RemoveByValue(int age)
+    {
+        for (int i = 0; i < ArrayUsers.Length; i++)
+        {
+            if (age == ArrayUsers[i].Age)
+            {
+                ArrayUsers[i].Name = "defaul";
+                ArrayUsers[i].Age = 0;
+            }
+        }
         return true;
     }
 
--- OOPHomeTask/Program.cs
+++ OOPHomeTask/Program.cs
@@ -1,7 +1,7 @@
 ﻿using System;
 using ClassData;
 
-namespace MyApp 
+namespace MyApp
 {
     internal class Program
     {
@@ -10,7 +10,12 @@ namespace MyApp
             DbUser array = new DbUser(5);
             array.AddUser(4, "viktor", 25);
             array.Show();
+            array.AddUser(2, "viktor123", 25);
+            array.AddUser(3, "vikto21312r123", 25);
+            array.Show();
             array.RemoveById(4);
+            array.RemoveByValue("viktor123");
+            Console.WriteLine(array.Search("vikto21312r123"));
             array.Show();
         }
     }
