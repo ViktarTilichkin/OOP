325ca1dae5369c5ee12e5df5909b709ad09a968e
ViktarTilichkin <tilichkin@mail.ru>
2023-01-26 22:07:53 +0300

    update

--- OOPHomeTask/Program.cs
+++ OOPHomeTask/Program.cs
@@ -7,16 +7,59 @@ namespace MyApp
     {
         static void Main(string[] args)
         {
-            var array = new CustomClass<string>(5);
-            array.Show();
-            array.AddValue("hello");
-            array.AddValue("world");
-            array.AddValue("and");
-            array.Show();
-            array.RemoveByIndex(0);
-            array.RemoveByValue("and");
-            Console.WriteLine(array.SearchIndex("world"));
-            array.Show();
+            //var array = new CustomClass<string>(5);
+            //array.Show();
+            //array.AddValue("hello");
+            //array.AddValue("world");
+            //array.AddValue("and");
+            //array.Show();
+            //array.RemoveByIndex(0);
+            //array.RemoveByValue("and");
+            //Console.WriteLine(array.SearchIndex("world"));
+            //array.Show();
+            List<int> array = new List<int>(20);
+            System.Console.WriteLine($"Capacity={array.Capacity} countItem={array.Count}");
+            array.Add(1);
+            System.Console.WriteLine($"Capacity={array.Capacity} countItem={array.Count}");
+            array.Add(1);
+            array.Add(11);
+            array.Add(1123);
+            array.Add(41);
+            array.Add(16);
+            System.Console.WriteLine($"Capacity={array.Capacity} countItem={array.Count}");
+            array.Add(1333);
+            array.Add(1);
+            array.Add(112);
+            array.Add(98);
+            array.Add(123);
+            array.Add(112);
+            array.Add(1231);
+            array.Add(8);
+            array.Add(6);
+            array.Add(13);
+            array.Add(163);
+            array.Add(5);
+            array.Add(67);
+            array.Add(9);
+            array.Add(78);
+            System.Console.WriteLine($"Capacity={array.Capacity} countItem={array.Count}");
+            Console.WriteLine(array.FirstOrDefault(x => x > 1));
+            Console.WriteLine(array.FindIndex(x => x > 4 ));
+            for(int i= 0; i < array.Count; i++) 
+            {
+                if (array[i] == 5)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine(string.Join("; ", array));
+            array.Sort();
+            Console.WriteLine(string.Join("; ", array));
+            Console.WriteLine("firstordefault");
+            int result = array.FirstOrDefault(item => item > 10);
+            Console.WriteLine($"firstordefault {result}");
+            Func<int, int, int> ourDel = (x, y) => x + y;
+            Console.WriteLine(ourDel(1,2));
         }
     }
 }
