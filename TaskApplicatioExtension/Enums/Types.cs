f6fbdda881c4e9be7adcd0f0da533c79548e195b
ViktarTilichkin <tilichkin@mail.ru>
2023-02-01 23:13:24 +0300

    update

---
+++ TaskApplicatioExtension/Enums/Types.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskApplicatioLinq.Enums
+{
+    public enum FileTypes
+    {
+        pdf = 1, txt = 2, doc = 3, html = 4, img = 5, svg = 6
+    }
+}
---
+++ TaskApplicatioExtension/Extensions/ObjectExtension.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+// 2. Написать метод расширения типа object, который возращает true, если можно
+// преобразовать в указанный тип данных и преобразованное значение, в противном случае
+// false. Метод должен называться TryToConvert()
+
+namespace TaskApplicatioLinq.Extensions
+{
+    public static class ObjectExtension
+    {
+        public static bool TryToConvert<T>(this object obj, out T result)
+        {
+            if (obj is T temp)
+            {
+                result = temp;
+                return true;
+            }
+            result = default;
+            return false;
+        }
+    }
+}
