4f28e52ed82bf42475daa2e92f3b2d05fc399aad
ViktarTilichkin <tilichkin@mail.ru>
2023-02-01 18:59:33 +0300

    update:

---
+++ TaskApplicatioLinq/TaskApplicatioLinq/Extensions/StingExtension.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+// 1. Написать метод раширения, который преобразует тип string в тип int с проверкой строки на
+// наличие только чисел
+
+
+namespace TaskApplicatioLinq.Extensions
+{
+    public static class StringExtension
+    {
+        public static int ToInt(this string str)
+        {
+            string result = "";
+            foreach (char c in str)
+            {
+                if (char.IsDigit(c))
+                {
+                    result += c;
+                }
+            }
+            return string.IsNullOrEmpty(result) ? 0 : int.Parse(result);
+        }
+
+    }
+}
